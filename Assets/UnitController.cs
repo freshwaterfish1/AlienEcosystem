2c12a7c73f1cf58fe92463505be68c6aac7db7a2
Levente Nagy <freshwaterfish1@gmail.com>
2020-04-13 17:16:29 -0400

    Better Ai and a lot more public vars

--- Assets/UnitController.cs
+++ Assets/UnitController.cs
@@ -21,7 +21,12 @@ public class UnitController : MonoBehaviour
     // Start is called before the first frame update
     public float energy = 100.0f;
     public float speed = 3.0f;
+    public float acceleration = 3.0f;
+    public float turnspeed = 3.0f;
     public float sensoryRange = 10.0f;
+
+    public Collider[] hitColliders;
+
     public Vector3 targetDestination;
 
     float consumptionrate = 1.0f;
@@ -38,7 +43,13 @@ public class UnitController : MonoBehaviour
         timer += Time.deltaTime;
         energy -= (Time.deltaTime * consumptionrate);
 
-        if(energy <= 0)
+
+        // Set Navmesh Agent setting
+        agent.speed = speed;
+        agent.angularSpeed = turnspeed;
+        agent.acceleration = acceleration;
+
+        if (energy <= 0)
         {
             Debug.Log(gameObject + ("has died"));
             
@@ -49,6 +60,11 @@ public class UnitController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Q))
         {
             targetDestination = new Vector3((Random.Range(-itemXSpread, itemXSpread)), (Random.Range(-itemYSpread, itemYSpread)), (Random.Range(-itemZSpread, itemZSpread)));
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            hitColliders = Physics.OverlapSphere(gameObject.transform.position, sensoryRange);
         }
 
     }
